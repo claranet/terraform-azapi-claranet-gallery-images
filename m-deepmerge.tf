0a30a5d94543b1b430d2c01908b470b67075e88a
Jérémy MARMOL <jeremy.marmol@claranet.com>
2025-02-14 17:38:25 +0100

    chore: apply suggestion

--- m-deepmerge.tf
+++ m-deepmerge.tf
@@ -1,7 +1,7 @@
 module "deepmerge_debian" {
   source  = "Invicton-Labs/deepmerge/null"
   version = "0.1.5"
-  # insert the 1 required variable here
+
   maps = [
     local.image_versions["debian"],
     local.image_versions["debian-sec"]
@@ -11,7 +11,7 @@ module "deepmerge_debian" {
 module "deepmerge_ubuntu" {
   source  = "Invicton-Labs/deepmerge/null"
   version = "0.1.5"
-  # insert the 1 required variable here
+
   maps = [
     local.image_versions["ubuntu"],
     local.image_versions["ubuntu-sec"]
@@ -21,7 +21,7 @@ module "deepmerge_ubuntu" {
 module "deepmerge_windows" {
   source  = "Invicton-Labs/deepmerge/null"
   version = "0.1.5"
-  # insert the 1 required variable here
+
   maps = [
     local.image_versions["windows"],
     local.image_versions["windows-sec"]
